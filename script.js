8a222e160cc10162dffd5209ed1f158e14e450dd
Gavin <gavinvm@protonmail.com>
2023-11-22 15:46:06 +0200

    Remove block function

--- script.js
+++ script.js
@@ -16,24 +16,13 @@ function createRows() {
         rowContainer.style.cssText = 'border-bottom: 1px solid black';    
         rowContainer.classList.add('rows');
         sketchPad.appendChild(rowContainer);
-    }
 
-    createBlocks(numberOfRows);
-}
-
-function createBlocks(number) {
-
-    const rows = document.getElementsByClassName('rows');
-
-    for (let i = 0; i < number; i++) {
-
-        for (let j = 0; j < number; j++) {
+        for (let j = 0; j < numberOfRows; j++) {
 
             const block = document.createElement('div');
             block.style.cssText = 'border-right: 1px solid black';
             block.classList.add('block');
-
-            rows[i].appendChild(block);
+            rowContainer.appendChild(block);
         }
     }
 }

8583839727882f52defaa45bede4f0464d505fca
Gavin <gavinvm@protonmail.com>
2023-11-22 15:41:39 +0200

    Create block elements

--- script.js
+++ script.js
@@ -17,5 +17,23 @@ function createRows() {
         rowContainer.classList.add('rows');
         sketchPad.appendChild(rowContainer);
     }
-    
+
+    createBlocks(numberOfRows);
+}
+
+function createBlocks(number) {
+
+    const rows = document.getElementsByClassName('rows');
+
+    for (let i = 0; i < number; i++) {
+
+        for (let j = 0; j < number; j++) {
+
+            const block = document.createElement('div');
+            block.style.cssText = 'border-right: 1px solid black';
+            block.classList.add('block');
+
+            rows[i].appendChild(block);
+        }
+    }
 }
